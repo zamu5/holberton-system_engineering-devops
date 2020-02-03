53fa1ac32d07ac1b5a41c79351ce196ad040912e
Sergio Zamudio <szamu5@hotmail.com>
2020-02-03 12:26:06 -0500

    FEATURE: save the data of the previus task in a cvs file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Returns information about his/her TODO list progress.
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        sys.argv[1])
+    json_res = requests.get(url).json()
+    tasks = ""
+    name = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
+        sys.argv[1])).json()
+    file_name = "{}.csv".format(sys.argv[1])
+    with open(file_name, 'w') as cvs_file:
+        for task in json_res:
+            tasks = ",".join(['"{}"'.format(name.get("id")),
+                              '"{}"'.format(name.get("username")),
+                              '"{}"'.format(task.get("completed")),
+                              '"{}"'.format(task.get("title"))])
+            cvs_file.write(tasks + "\n")
