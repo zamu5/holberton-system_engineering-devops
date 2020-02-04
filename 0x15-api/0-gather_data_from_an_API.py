2162fdccde2ab077b3e4922ccf97327aa6ca4ddb
Sergio Zamudio <szamu5@hotmail.com>
2020-02-03 20:11:39 -0500

    DOC: module documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# Returns information about his/her TODO list progress.
+"""Returns information about his/her TODO list progress."""
 
 import requests
 import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    json_res = requests.get(url + "todos", params={'userId': sys.argv[1]}).json()
+    json_res = requests.get(url + "todos",
+                            params={'userId': sys.argv[1]}).json()
     tasks = []
     task_count = len(json_res)
     for task in json_res:
@@ -14,7 +15,7 @@ if __name__ == "__main__":
             tasks.append("\t " + task.get("title"))
     json_res = requests.get(url + "users/{}".format(sys.argv[1])).json()
     print("Employee {} is done with tasks({}/{}):".format(json_res.get("name"),
-                                                         len(tasks),
-                                                         task_count))
+                                                          len(tasks),
+                                                          task_count))
     for row in tasks:
         print(row)
