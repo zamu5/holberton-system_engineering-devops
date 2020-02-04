d4309352125e357799a5b60f3487332d9128f4e0
Sergio Zamudio <szamu5@hotmail.com>
2020-02-03 20:08:16 -0500

    FIX: delete the counter of tasks done.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,15 +8,13 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     json_res = requests.get(url + "todos", params={'userId': sys.argv[1]}).json()
     tasks = []
-    task_done = 0
     task_count = len(json_res)
     for task in json_res:
         if task.get("completed") is True:
             tasks.append("\t " + task.get("title"))
-            task_done += 1
     json_res = requests.get(url + "users/{}".format(sys.argv[1])).json()
     print("Employee {} is done with tasks({}/{}):".format(json_res.get("name"),
-                                                         task_done,
+                                                         len(tasks),
                                                          task_count))
     for row in tasks:
         print(row)
